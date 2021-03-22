645abfdd1def7f94d701d39c3e7a9ed9dbc24d00
TY-Shambhavi-V <shambhavi.v@testyantra.in>
2021-03-22 12:52:35 +0530

    Updated Taborder

--- src/api/abstract/FormDesigner/FdDialogDragVue.ts
+++ src/api/abstract/FormDesigner/FdDialogDragVue.ts
@@ -18,8 +18,11 @@ export default class FdDialogDragVue extends Vue {
        top: '50px'
      };
      isTabOrderOpen: boolean = false
-     currentIndex: number = -1
+     currentIndex: number[] = []
      tabOrderList: localTabOrderItem[] | tabsItems[] = []
+     isMultiple: boolean
+     shiftUp: boolean = false
+     shiftDown: boolean = false
      protected dragTabOrderDialog (event: MouseEvent) {
        this.positions.clientX = event.clientX
        this.positions.clientY = event.clientY
@@ -46,27 +49,129 @@ export default class FdDialogDragVue extends Vue {
        document.onmousemove = null
      }
      protected selectedTab (data: number) {
-       this.currentIndex = data
+       if (this.isMultiple) {
+         this.currentIndex = [...this.currentIndex, data]
+       } else {
+         this.currentIndex = [data]
+       }
      }
-     protected swapTabOrderList (aIndex: number, bIndex: number) {
-       const temp = this.tabOrderList[aIndex]
-       this.tabOrderList[aIndex] = this.tabOrderList[bIndex]
-       this.tabOrderList[bIndex] = temp
+     protected onDrag (data:number, e:MouseEvent) {
+       if (e.which === 1) {
+         let istrue = this.currentIndex.includes(data)
+         if (this.currentIndex[0] !== data) {
+           this.currentIndex = [...this.currentIndex, data]
+         } else {
+           this.currentIndex = [data]
+         }
+         if (istrue && this.currentIndex.length > 2) {
+           this.currentIndex = this.currentIndex.slice(0, this.currentIndex.length - 2)
+         }
+       }
+     }
+     protected selectTabOnKeyDown (value:object, data:number, e:KeyboardEvent) {
+       if (e.shiftKey && e.key === 'ArrowUp') {
+         let count = 0
+         if (!this.shiftDown) {
+           this.currentIndex = [...this.currentIndex, this.currentIndex[0] - 1]
+         } else {
+           if (this.currentIndex.length > 1) {
+             this.currentIndex.pop()
+           } else {
+             this.currentIndex.push(this.currentIndex[0] - 1)
+             this.shiftDown = false
+           }
+         }
+         this.selectItem(count)
+         this.shiftUp = true
+       }
+       if (e.shiftKey && e.key === 'ArrowDown') {
+         let count = 0
+         if (!this.shiftUp) {
+           this.currentIndex.reverse()
+           this.currentIndex = [...this.currentIndex, this.currentIndex[0] + 1]
+         } else {
+           this.currentIndex.reverse()
+           if (this.currentIndex.length > 1) {
+             this.currentIndex.pop()
+           } else {
+             this.currentIndex.push(this.currentIndex[0] + 1)
+             this.shiftUp = false
+           }
+         }
+         this.selectItem(count)
+         this.shiftDown = true
+       }
+       if (!e.shiftKey && e.key === 'ArrowUp') {
+         if (this.currentIndex[0] !== 0 && this.currentIndex[0] > 0) {
+           this.selectedTab(this.currentIndex[0] - 1)
+         } else {
+           this.selectedTab(this.currentIndex[0])
+         }
+       }
+       if (!e.shiftKey && e.key === 'ArrowDown') {
+         if (this.currentIndex[0] !== this.tabOrderList.length - 1) {
+           this.selectedTab(this.currentIndex[0] + 1)
+         } else {
+           this.selectedTab(this.currentIndex[0])
+         }
+       }
+     }
+     protected selectItem (count: number) {
+       this.currentIndex.sort()
+       for (let index of this.currentIndex) {
+         count = count + 1
+         if (count >= 2) {
+           this.isMultiple = true
+         }
+         this.selectedTab(index)
+       }
+       this.isMultiple = false
+     }
+     protected swapTabOrderList (currentIndex: number[], bIndex: number) {
+       for (let index of currentIndex) {
+         const temp = this.tabOrderList[bIndex]
+         this.tabOrderList[bIndex] = this.tabOrderList[index]
+         this.tabOrderList[index] = temp
+         bIndex++
+       }
+     }
+     protected swapTabOrderListOnDown (currentIndex: number[], bIndex: number) {
+       for (let index of currentIndex) {
+         const temp = this.tabOrderList[bIndex]
+         this.tabOrderList[bIndex] = this.tabOrderList[index]
+         this.tabOrderList[index] = temp
+       }
      }
      protected moveControlUp () {
-       const currentIndex = this.currentIndex
-       if (currentIndex !== 0) {
-         this.swapTabOrderList(currentIndex, currentIndex - 1)
-         this.selectedTab(currentIndex - 1)
+       let currentIndex = this.currentIndex.sort()
+       let count = 0
+       if (currentIndex[0] !== 0) {
+         this.swapTabOrderList(currentIndex, currentIndex[0] - 1)
+         for (let index of currentIndex) {
+           count = count + 1
+           if (count >= 2) {
+             this.isMultiple = true
+           }
+           this.selectedTab(index - 1)
+         }
+         this.isMultiple = false
        }
      }
 
      protected moveControlDown () {
        const lastIndex = this.tabOrderList.length - 1
-       const currentIndex = this.currentIndex
-       if (currentIndex !== lastIndex) {
-         this.swapTabOrderList(currentIndex, currentIndex + 1)
-         this.selectedTab(currentIndex + 1)
+       const currentIndex = this.currentIndex.sort()
+       let count = 0
+       if (currentIndex[currentIndex.length - 1] !== lastIndex) {
+         this.swapTabOrderListOnDown(currentIndex, currentIndex[currentIndex.length - 1] + 1)
+         for (let index of currentIndex) {
+           count = count + 1
+           if (count >= 2) {
+             this.isMultiple = true
+           }
+           this.selectedTab(index + 1)
+         }
+         this.isMultiple = false
        }
      }
      protected closeDialog () {
